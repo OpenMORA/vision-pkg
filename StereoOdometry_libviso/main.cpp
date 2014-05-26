fe267026044eac31d772666c68b18e94d2202f49
Jose Luis Blanco <joseluisblancoc@gmail.com>
2014-05-26 13:02:30 +0200

    Templatized main.cpp's

--- StereoOdometry_libviso/main.cpp
+++ StereoOdometry_libviso/main.cpp
@@ -1,22 +1,11 @@
 /* +---------------------------------------------------------------------------+
    |                 Open MORA (MObile Robot Arquitecture)                     |
-   |                  University of Almeria ARM-eCar module                    |
-   |                                                                           |
-   |   Copyright (C) 2014  University of Almeria                               |
    +---------------------------------------------------------------------------+ */
 
-#include "CStereoOdometryApp.h"
+#include "CStereoOdometryApp.h" // User C++ app class 
 
-int main(int argc ,char * argv[])
-{
-	//here we do some command line parsing...
-	//mission file could be first free parameter
-	//mission name can be the  second free parameter
-	MOOS::CommandLineParser P(argc,argv);
-	std::string mission_file = P.GetFreeParameter(0, "Mission.moos");
-	std::string app_name = P.GetFreeParameter(1, "StereoOdometry_libviso");
+#define MORA_APP_CLASS         CStereoOdometryApp     
+#define MORA_APP_NAME          "StereoOdometry_libviso"    // Default MOOSApp app name
 
-    CStereoOdometryApp TheApp;
-    TheApp.Run(app_name,mission_file,argc,argv);
-    return 0;
-}
+#include <mora_main.h>
+
